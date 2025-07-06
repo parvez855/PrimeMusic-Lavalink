d4143ca3d8b3954eea400494ac64d11cdb61dec7
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-06 16:20:17 +0600

    support.js

--- commands/support.js
+++ commands/support.js
@@ -9,10 +9,10 @@ module.exports = {
     options: [],
     run: async (client, interaction, lang) => {
         try {
-            const supportServerLink = "https://discord.gg/xQF9f9yUEM";
-            const githubLink = "https://github.com/GlaceYT";
-            const replitLink = "https://replit.com/@GlaceYT";
-            const youtubeLink = "https://www.youtube.com/@GlaceYT";
+            const supportServerLink = "https://discord.gg/56uzCcbVDs";
+            const githubLink = "https://github.com/lstrxx1top";
+            const replitLink = "https://replit.com/@latexx1top";
+            const youtubeLink = "https://www.youtube.com/@latexx1top";
 
             const embed = new EmbedBuilder()
                 .setColor('#b300ff')
