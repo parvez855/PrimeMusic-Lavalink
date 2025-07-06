bee6fb24b14716b2423d6efb5b261a5f9a348e08
parvez855 <130054027+parvez855@users.noreply.github.com>
2025-07-06 15:40:15 +0600

    config.js

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
   TOKEN: "",
   language: "en",
   ownerID: ["1004206704994566164", ""], 
-  mongodbUri : "",
+  mongodbUri : "mongodb+srv://rahul:rahul@cluster0.cpj0azk.mongodb.net/?retryWrites=true&w=majority",
   spotifyClientId : "",
   spotifyClientSecret : "",
   setupFilePath: './commands/setup.json',
